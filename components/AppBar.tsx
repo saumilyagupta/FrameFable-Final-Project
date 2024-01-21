e70a8877a6a15c0d124a1eed14b2110b09c77fa9
“[Harshit <[harshitacademia@gmail.com]>
2024-01-21 10:36:36 +0530

    Initial commit

---
+++ components/AppBar.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from '../styles/Home.module.css'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import Image from 'next/image'
+
+export const AppBar: FC = () => {
+    return (
+        <div className={styles.AppHeader}>
+            
+            <span>FrameFables</span>
+            <WalletMultiButton />
+        </div>
+    )
+}
